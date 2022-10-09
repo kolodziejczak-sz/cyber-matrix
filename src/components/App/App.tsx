f1bf58082859fd52550493c9b214eb4fbc6f94e6
szymon <s.kolodziejczak95@gmail.com>
2022-10-09 14:09:38 +0200

    move generation to App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,3 +1,6 @@
+import { getRandomInteger } from '@/utils/getRandomInteger';
+import { GameSettings } from '@/game/types';
+import { initContext } from '@/game/context';
 import { Timer } from '@/components/Timer';
 import { Buffer } from '@/components/Buffer';
 import { Matrix } from '@/components/Matrix';
@@ -6,6 +9,26 @@ import { Sequences } from '@/components/Sequences';
 import './App.css';
 
 export const App = () => {
+  const matrixLength = getRandomInteger(5, 7);
+  const bufferLength = getRandomInteger(4, 6);
+
+  const controllerQuery = window.matchMedia('(pointer: coarse)');
+
+  const gameSettings: GameSettings = {
+    controllerSettings: controllerQuery.matches ? 'touch' : 'mouse',
+    bufferSettings: { length: bufferLength },
+    matrixSettings: { rowLength: matrixLength },
+    scopeSettings: { index: 0, direction: 'row' },
+    sequencesSettings: [
+      { length: 2, points: 100, },
+      { length: 3, points: 200, },
+      { length: 4, points: 400, },
+    ],
+    timerSettings: { duration: 106_600 },
+  };
+  
+  initContext(gameSettings);
+
   return (
     <div class="app">
       <Timer className="app__timer" />
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,6 @@
 import { App } from '@/components/App';
-import { GameSettings } from '@/game/types';
-import { initContext } from '@/game/context';
 
 import './styles.css';
-
-const gameSettings: GameSettings = {
-  scopeSettings: { index: 0, direction: 'row' },
-  matrixSettings: { rowLength: 5 },
-  sequencesSettings: [
-    { length: 2, points: 100, },
-    { length: 3, points: 200, },
-    { length: 4, points: 400, },
-  ],
-  bufferSettings: { length: 5 },
-  timerSettings: { duration: 59_600 },
-};
-
-initContext(gameSettings);
 
 const root = document.getElementById('root');
 
